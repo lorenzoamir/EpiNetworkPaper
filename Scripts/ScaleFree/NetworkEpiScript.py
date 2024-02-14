a1c4c49f7fa25aea8480223a0db55de2b5abc15a
lollo <lorenemati@gmail.com>
2024-02-14 18:04:43 +0100

    new version

--- Scripts/ScaleFree/NetworkEpiScript.py
+++ Scripts/ScaleFree/NetworkEpiScript.py
@@ -9,6 +9,7 @@ import getopt
 import numpy as np
 import networkx as nx
 import random
+from scipy.sparse import triu
 
 # SCALE FREE
 
@@ -47,7 +48,7 @@ def get_activity(k_min, k_max, gamma):
     
     return a
 
-def create_network():
+def create_network(N, k_min, k_max, gamma):
 
     a=[]
     for i in range(N):
@@ -90,7 +91,8 @@ def create_network():
 
     return G
 
-dt = 0.1
+dt = 1
+a_steps = 10 # Number of social activity upgrades at each time step
 
 N_sims = 1000 # Number of simulations 1000
 N = 10000 # Number of individuals 1e4 
@@ -116,117 +118,198 @@ i0 = 0.01 # Fraction of initial infected nodes
 # frac = 10/N # Only keep runs where the disease reaches this fraction of the population
 t_max = round(1000/dt) # Max length of a simulation
 
-def simulate():
-    # Simulates the epidemic. The change in social activity is only computed for infected nodes and
-    # their neighbours
+def get_prev_i(all_links, i_tuple: tuple, k):
+    """
+    Returns the prevalence of infected nodes in the neighbors of each node the network
+    
+    Parameters:
+    all_links: array
+        An array of all the links in the network,
+        all_links = np.array(nx.adjacency_matrix(G).nonzero()).T
+    i_tuple: tuple
+        A tuple with all the infected nodes
+    k: array
+        An array with the degree of each node in the network
+        k = np.array(G.degree)[:,1]
+    """
+    
+    N = len(k)
 
-    G = create_network()
+    # only keep links where second node is infected
+    infected_only = all_links[np.isin(all_links[:,1], i_tuple)]
+
+    # count the number of infected neighbors for each node
+    counts = np.zeros(N)
+    has_i_neigh, counts[has_i_neigh] = np.unique(infected_only[:, 0], return_counts=True)
+
+    # divide counts by degree
+    prev = np.where(
+        np.isin(range(N), has_i_neigh),
+        counts / k,
+        0
+        )
+    
+    return prev
+
+def get_avg_a(all_links, a, k):
+    """
+    Returns the average social activity (a) of the neighbors of each node in the network
+
+    Parameters:
+    all_links: array
+        An array of all the links in the network,
+        all_links = np.array(nx.adjacency_matrix(G).nonzero()).T
+    a: array
+        An array with the social activity of each node in the network
+    k: array
+        An array with the degree of each node in the network
+        k = np.array(G.degree)[:,1]
+    """
+
+    N = len(k)
+
+    # get social activity of each second node in the links
+    a_neighs = a[all_links[:,1]]
+
+    # sum the social activity of the neighbors for each node
+    # the social activity of the neighbors is stored in a_neighs,
+    # the number of neighbors is stored in k
+    a_sum = np.bincount(all_links[:,0], weights=a_neighs)
+
+    # divide by degree
+    avg_a = a_sum / k
+
+    return avg_a
+
+def get_prev_s(all_links, i_tuple: tuple, r_tuple: tuple, k):
+    """
+    Returns the prevalence of susceptible nodes in the neighbors of each node the network
+
+    Parameters:
+    all_links: array
+        An array of all the links in the network,
+        all_links = np.array(nx.adjacency_matrix(G).nonzero()).T
+    i_tuple: tuple
+        A tuple with all the infected nodes
+    r_tuple: tuple
+        A tuple with all the recovered nodes
+    k: array
+        An array with the degree of each node in the network
+        k = np.array(G.degree)[:,1]
+    """
+
+    N = len(k)
+    
+    s_tuple = np.setdiff1d(range(N), np.concatenate((i_tuple, r_tuple)))
+    # only keep links where second node is susceptible
+    susceptible_only = all_links[np.isin(all_links[:,1], s_tuple)]
+
+    # count the number of susceptible neighbors for each node
+    counts = np.zeros(N)
+    has_s_neigh, counts[has_s_neigh] = np.unique(susceptible_only[:, 0], return_counts=True)
+
+    # divide counts by degree
+    prev = np.where(
+        np.isin(range(N), has_s_neigh),
+        counts / k,
+        0
+        )
+    
+    return prev
+
+def simulate(N, k_min, k_max, gamma, i0, t_max, beta_default, mu, alpha=0, seed=42, N_steps=10):
+
+    G = create_network(N, k_min, k_max, gamma)
+
+    k = np.array(G.degree)[:,1]
+
     k_ave = 2*len(G.edges)/N
     beta = beta_default/k_ave
 
-    # Init
-    infected_nodes = set(random.sample([node for node in G.nodes()], max(1, round(N * i0))))
+    # Init all nodes to S
+    disease_status=np.array(["s"] * N)
+
+    # Select i0*N nodes to be infected
+    i_set = set(random.sample(range(0,N), max(1, round(N * i0))))
+    r_set = set()
+
+    # Init social activity (a) to 1
+    a = np.ones(N)
+
+    # non-zero elements are links, (symmetric)
     
-    for i in G:
-        # Set social activity to 1 and nodes to their initial status
-        G.a[i] = 1
-        if i in infected_nodes:
-            G.disease_status[i] = "i"
-        else:
-            G.disease_status[i] = "s"
+    # All links (each link appears twice)
+    all_links = np.array(nx.adjacency_matrix(G).nonzero()).T # 
 
-    result = {"s": [1 - len(infected_nodes)/N],
-              "i": [len(infected_nodes)/N],
-              "r": [0]}
-    
-    t = 0
-    
-    effective_nodes = set() # Only I nodes and their neighbours are relevant
+    # only use the upper triangle (diagonal excluded)
+    links_no_dupl = np.array(triu(nx.adjacency_matrix(G)).nonzero()).T
 
-    for i in infected_nodes:
-        if i not in effective_nodes:
-            effective_nodes.add(i)
-        for j in G.neighbors(i):
-            if j not in effective_nodes:
-                effective_nodes.add(j)
+    result = {
+        "s": [(N - len(i_set)) / N],
+        "i": [len(i_set)/N],
+        "r": [0]
+        }
 
-    for t in range(0, t_max):
+    for t in range(t_max):
 
-        dSI = 0
-        dIR = 0
+        i_new = set()
+        r_new = set()
+
+        i_tuple = tuple(i_set)
+        r_tuple = tuple(r_set)
+
+        prev = get_prev_i(all_links, i_tuple, k) # prevalence
+        sigma = get_prev_s(all_links, i_tuple, r_tuple, k) # prob of being S
+        avg_a = get_avg_a(all_links, a, k) # avg social activity
+
+        for _ in range(N_steps):
+            a = 1 / (1 + alpha * delta * beta_default * sigma * prev * avg_a)
+
+        # Remove links in which at least one node is recovered
+        active_links = links_no_dupl[~np.isin(links_no_dupl, r_tuple).any(axis=1)]
+        # Subset to links where only one node is infected
+        active_links = active_links[np.sum(np.isin(active_links, i_tuple), axis=1) == 1]
+
+        # Generate random numbers for each active link
+        rand = np.random.rand(len(active_links))
+
+        # save new infections
+        i_thr = beta*a[active_links[:,0]] * a[active_links[:,1]]
+
+        i_new = set(np.unique(active_links[rand < i_thr])) - i_set
+
+        # Extract number of recoveries from a binomial distribution
+        n_recoveries = np.random.binomial(len(i_set), mu)
+
+        # Randomly select indices of nodes to recover
+        rand = np.random.choice(len(i_set), n_recoveries, replace=False)
+
+        r_new = set([i_tuple[i] for i in rand])
+
+        # add new infections to infected nodes
+        if i_new:
+            i_set = i_set | i_new
+
+        if r_new:
+            # remove recoveries from infected nodes
+            i_set = i_set - r_new
+            # add new recoveries to recovered nodes
+            r_set = r_set | r_new
+
+        result["s"].append((N - len(i_set) - len(r_set)) / N)
+        result["i"].append(len(i_set) / N)
+        result["r"].append(len(r_set) / N)
+
+        if len(i_set) == 0:
+                # Repeat the last value of s, i and r until the end if the epidemics ends before tmax
+                result["s"] = np.pad(result["s"], [(0, t_max+1-len(result["s"]))], mode='edge')
+                result["i"] = np.pad(result["i"], [(0, t_max+1-len(result["i"]))], mode='constant') # pad 0
+                result["r"] = np.pad(result["r"], [(0, t_max+1-len(result["r"]))], mode='edge')
+
+                break 
         
-        # Update theta
-        for i in effective_nodes:
-            G.theta[i] = sum([G.a[j] if G.disease_status[j] == "i" else 0 for j in G.neighbors(i)])
-            G.s[i]     = sum([1      if G.disease_status[j] == "s" else 0 for j in G.neighbors(i)])/G.degree[i]
-        
-        # Update social activity
-        for i in effective_nodes:
-            G.a[i] = 1 / (1 + G.s[i]*beta*G.theta[i]*delta*(alpha/dt))
-        
-        infected_add  = set() # Will be added to infected
-        effective_add = set() # Will be added to effective
-        
-        for i in infected_nodes:
-            for j in G.neighbors(i):
-                if G.disease_status[j]=="s":
-                    p=np.random.random()
-                    if p<beta*G.a[i]*G.a[j]:
-                        G.disease_status[j]="i" # S -> I
-                        infected_add.add(j)
-                        effective_add.add(j)                       
-                        dSI += 1
-                        for k in G.neighbors(j): # Add new I node's neighbours to effective
-                            if G.disease_status[k] == "s": # Infected nodes are already effective
-                                effective_add.add(k)       # recovered nodes should not be effective 
-
-        infected_remove  = set() # Will be removed from infected
-        effective_remove = set() # Will be removed from effective
-        
-        for i in infected_nodes:
-            p = np.random.random()
-            if p<mu:
-                G.disease_status[i]="r" # I -> R
-                infected_remove.add(i) # remove from infected
-                effective_remove.add(i) # remove from effected
-                dIR += 1
-                for j in G.neighbors(i):
-                    if j in effective_nodes and G.disease_status[j] == "s":
-                        if "i" not in [G.disease_status[k] for k in G.neighbors(j)]:
-                            effective_remove.add(j)
-                            # If the list of the health statuses of the neighbours (k) of the neighbour (j)
-                            # of the recovered node (i) doesn't contain any infected node, then the node j
-                            # can be removed from the effective nodes.
-                            # We also need to check that the node is in the S state, otherwise it could be
-                            # an I node (which should be effective) even if it doesn't have I neighbours
-                
-        for i in infected_add:
-            infected_nodes.add(i)
-        for i in infected_remove:
-            infected_nodes.remove(i)
-            
-        for i in effective_add:
-            if i not in effective_nodes:
-                effective_nodes.add(i)
-        for i in effective_remove:
-                effective_nodes.remove(i)
-
-        result["s"].append(result["s"][-1] - dSI/N)
-        result["i"].append(result["i"][-1] + dSI/N - dIR/N)
-        result["r"].append(result["r"][-1] + dIR/N)
-
-        t+=1
-    
-        if len(infected_nodes) == 0:
-            # Repeat the last value of s, i and r until the end if the epidemics ends before tmax
-            result["s"] = np.pad(result["s"], [(0, t_max+1-len(result["s"]))], mode='edge')
-            result["i"] = np.pad(result["i"], [(0, t_max+1-len(result["i"]))], mode='constant') # pad 0
-            result["r"] = np.pad(result["r"], [(0, t_max+1-len(result["r"]))], mode='edge')
-
-            break 
-    
     tt = np.linspace(0, t_max, t_max+1)
-    
+        
     return tt, result
 
 # Let's create a matrix "sims_matrix" with N_sims rows and t_max columns,
@@ -238,7 +321,19 @@ N_keep = 0 # Number of runs in which i is over the threshold
 r_inf = []  # Final attack rates list
 
 for i, row in enumerate(sims_matrix):
-    tt, result = simulate()
+    tt, result = simulate(
+        N,
+        k_min,
+        k_max,
+        gamma,
+        i0,
+        t_max,
+        beta_default,
+        mu,
+        alpha=alpha,
+        seed=i,
+        N_steps=a_steps
+    )
 
 #    if(result["r"][-1] >= frac): # Only keep runs where the desease reaches a significant fraction of the pupulation
     row[:] = result["i"][:]
